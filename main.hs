f28d8b033ac658973582db6d92efa22f6aa0f78d
theguruofreason <theguruofreason@gmail.com>
2014-04-19 02:44:19 -0700

    fuckery

--- main.hs
+++ main.hs
@@ -17,7 +17,7 @@ main = do
   (_, _, spriteSheet) <- loadDevILPicture ("." </> "resources" </> "Belal_Smooth_Walls.png")
   let
       window = InWindow "test 1" (640, 480) (10, 10)
-  play window black 60 spriteSheet (defaultWorld
+  play window black 60 spriteSheet (defaultWorld spriteSheet)
 
 
 data World = World { _characterPosition :: (Int, Int)
