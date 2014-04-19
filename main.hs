93c8433c990bd9b998529278fbb8b6952425ddb4
theguruofreason <theguruofreason@gmail.com>
2014-04-19 02:52:05 -0700

    fixed

--- main.hs
+++ main.hs
@@ -12,24 +12,24 @@ import           Graphics.Gloss.DevIL
 import           Items
 import           System.FilePath
 
+data World = World { _characterPosition :: (Int, Int)
+                   , _characterSprite :: Picture
+                   , _sprites :: Picture
+                   }
+$(makeLenses ''World)
+
+defaultWorld :: Picture -> World
+defaultWorld image = World { _characterPosition = (10,10)
+                           , _characterSprite = circleSolid 20
+                           , _sprites = image
+                           }
+
+drawWorld :: World -> Picture
+drawWorld world = world^.sprites
+
 main :: IO ()
 main = do
   (_, _, spriteSheet) <- loadDevILPicture ("." </> "resources" </> "Belal_Smooth_Walls.png")
   let
       window = InWindow "test 1" (640, 480) (10, 10)
-  play window black 60 spriteSheet (defaultWorld spriteSheet)
-
-
-data World = World { _characterPosition :: (Int, Int)
-                   , _characterSprite :: Picture
-                   , _spriteSheet :: Picture
-                   }
-$(makeLenses ''World)
-
-defaultWorld image = World { _characterPosition = (10,10)
-                           , _characterSprite = circleSolid 20
-                           , _spriteSheet = image
-                           }
-
-drawWorld :: World -> Picture
-drawWorld world = world^.spriteSheet
+  play window black 60 (defaultWorld spriteSheet) drawWorld undefined undefined
