8a30eae438e82265d87a26a1170fb3cc2a4758e3
theguruofreason <theguruofreason@gmail.com>
2014-04-19 02:56:14 -0700

    runs now

--- main.hs
+++ main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE TemplateHaskell #-}
+import           Control.Lens
 import           Data.Array.Repa
 import           Data.Array.Repa.IO.DevIL
 import           Data.Array.Repa.Repr.ForeignPtr
 import           Data.Array.Repa.Shape
-import Control.Lens
 import           Data.Maybe
 import           Entities
 import           Foreign.ForeignPtr
@@ -13,8 +13,8 @@ import           Items
 import           System.FilePath
 
 data World = World { _characterPosition :: (Int, Int)
-                   , _characterSprite :: Picture
-                   , _sprites :: Picture
+                   , _characterSprite   :: Picture
+                   , _sprites           :: Picture
                    }
 $(makeLenses ''World)
 
@@ -32,4 +32,4 @@ main = do
   (_, _, spriteSheet) <- loadDevILPicture ("." </> "resources" </> "Belal_Smooth_Walls.png")
   let
       window = InWindow "test 1" (640, 480) (10, 10)
-  play window black 60 (defaultWorld spriteSheet) drawWorld undefined undefined
+  play window black 60 (defaultWorld spriteSheet) drawWorld (flip const) (flip const)
