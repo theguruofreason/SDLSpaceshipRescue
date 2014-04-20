9dd6e641abce4984dfd386c67177d39cd30bd609
theguruofreason <theguruofreason@gmail.com>
2014-04-19 20:59:34 -0700

    starting keyhandling

--- main.hs
+++ main.hs
@@ -18,24 +18,30 @@ data World = World { _characterPosition :: (Int, Int)
                    }
 $(makeLenses ''World)
 
+tileTranslate tiles = 12 * tiles
+ 
 defaultWorld :: Picture -> World
-defaultWorld image = World { _characterPosition = (10,10)
+defaultWorld image = World { _characterPosition = (tileTranslate 5, tileTranslate 5)
                            , _characterSprite = circleSolid 20
                            , _sprites = image
                            }
 
 drawWorld :: World -> Picture
-drawWorld world = world^.sprites
+drawWorld world = translate (world^.characterPosition_1) (world^.characterPosition_2) world^.sprites
 
 
 main :: IO ()
 main = do
   (RGBA arr) <- readRepaImage ("." </> "resources" </> "Nice_curses_12x12.png")
-  print . listOfShape . extent $ arr
   let
       window = InWindow "test 1" (800, 640) (0, 0)
       (offset, size) = (shapeOfList [4,12,180], shapeOfList [4, 12, 12])
-      (w,h,pic) = imageToPicture True . RGBA . computeS $ extract offset size arr
-      {-charSprite = imageToPicture $ computeS $ extract (fst charShape) (snd charShape) spriteSheet-}
-  play window black 60 (defaultWorld (scale 2 3 pic)) drawWorld (flip const) (flip const)
+      (w,h,charSprite) = imageToPicture True . RGBA . computeS $ extract offset size arr
+  play window black 60 (defaultWorld (scale 2 3 charSprite)) drawWorld keyHandler (flip const)
 
+keyHandler key world =
+    case key of
+      KeyUp Down -> world { _characterPosition = (world^.characterPosition_1, world^.characterPosition_2 + tileTranslate 1)
+      KeyDown Down -> world { _characterPosition = (world^.characterPosition_1, world^.characterPosition_2 1 tileTranslate 1)
+      KeyLeft Down -> world { _characterPosition = (world^.characterPosition_1 - tileTranslate 1, world^.characterPosition_2)
+      KeyRight Down -> world { _characterPosition = (world^.characterPosition_1 + tileTranslate 1, world^.characterPosition_2)
