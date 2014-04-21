9ff9c13a4b4ba02e45203fd2fbc45675f7b5690f
theguruofreason <theguruofreason@gmail.com>
2014-04-20 23:09:26 -0700

    added support for numpad (doesn't work though)

--- main.hs
+++ main.hs
@@ -4,28 +4,27 @@ import           Data.Array.Repa
 import           Data.Array.Repa.IO.DevIL
 import           Data.Array.Repa.Repr.ForeignPtr
 import           Data.Array.Repa.Shape
+import           Data.Map
 import           Data.Maybe
 import           Entities
 import           Foreign.ForeignPtr
 import           Graphics.Gloss
 import           Graphics.Gloss.DevIL
-import Graphics.Gloss.Interface.Pure.Game
-import           Items
+import           Graphics.Gloss.Interface.Pure.Game
 import           System.FilePath
-import Data.Map
 
-data World = World { _characterPosX     :: Float
-                   , _characterPosY     :: Float
-                   , _characterSprite   :: Picture
-                   , _sprites           :: Picture
+data World = World { _characterPosX   :: Float
+                   , _characterPosY   :: Float
+                   , _characterSprite :: Picture
+                   , _sprites         :: Picture
                    }
 $(makeLenses ''World)
 
-tileTranslate tiles = 24 * tiles
+tileScale tiles = 24 * tiles
 
 defaultWorld :: Picture -> World
-defaultWorld image = World { _characterPosX = tileTranslate 5
-                           , _characterPosY = tileTranslate 5
+defaultWorld image = World { _characterPosX = tileScale 5
+                           , _characterPosY = tileScale 5
                            , _characterSprite = circleSolid 20
                            , _sprites = image
                            }
@@ -36,11 +35,11 @@ drawWorld world = translate (world^.characterPosX) (world^.characterPosY) (world
 
 main :: IO ()
 main = do
-  (RGBA arr) <- readRepaImage ("." </> "resources" </> "Nice_curses_12x12.png")
+  (RGBA spriteSheet) <- readRepaImage ("." </> "resources" </> "Nice_curses_12x12.png")
   let
       window = InWindow "test 1" (800, 640) (0, 0)
       (offset, size) = (shapeOfList [4,12,180], shapeOfList [4, 12, 12])
-      (w,h,charSprite) = imageToPicture True . RGBA . computeS $ extract offset size arr
+      (w,h,charSprite) = imageToPicture True . RGBA . computeS $ extract offset size spriteSheet
   play window black 60 (defaultWorld (scale 2 2 charSprite)) drawWorld keyHandler (flip const)
 
 keymap :: Map (Key,KeyState) (World -> World)
@@ -48,12 +47,21 @@ keymap = fromList [ ((SpecialKey KeyUp, Down), characterMove 0 1 )
                   , ((SpecialKey KeyDown, Down), characterMove 0 (-1))
                   , ((SpecialKey KeyLeft, Down), characterMove (-1) 0)
                   , ((SpecialKey KeyRight, Down),  characterMove 1 0)
+                  , ((SpecialKey KeyPad1, Down), characterMove (-1) (-1))
+                  , ((SpecialKey KeyPad2, Down), characterMove 0 (-1))
+                  , ((SpecialKey KeyPad3, Down), characterMove 1 (-1))
+                  , ((SpecialKey KeyPad4, Down), characterMove (-1) 0)
+                  , ((SpecialKey KeyPad5, Down), characterMove 0 0)
+                  , ((SpecialKey KeyPad6, Down), characterMove 1 0)
+                  , ((SpecialKey KeyPad7, Down), characterMove (-1) 1)
+                  , ((SpecialKey KeyPad8, Down), characterMove 0 1)
+                  , ((SpecialKey KeyPad9, Down), characterMove 1 1)
                   ]
     where
-      characterMove xMove yMove world= world { _characterPosX = world^.characterPosX + tileTranslate xMove
-                                              , _characterPosY = world^.characterPosY + tileTranslate yMove
+      characterMove xMove yMove world = world { _characterPosX = world^.characterPosX + tileScale xMove
+                                              , _characterPosY = world^.characterPosY + tileScale yMove
                                               }
-  
+
 keyHandler :: Event -> World -> World
 keyHandler (EventKey k ks _ _) = findWithDefault id (k, ks) keymap
 keyHandler (EventMotion _) = id
